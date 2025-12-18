c456cadd86f70d8e3cca19509296db3464040bd1
Amit Divekar <110670491+uffamit@users.noreply.github.com>
2025-12-18 07:43:24 +0000

    k

--- next.config.mjs
+++ next.config.mjs
@@ -17,6 +17,16 @@ const nextConfig = {
       },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        // This captures all Firebase Auth internal paths
+        source: '/__/auth/:path*',
+        // Proxy requests to Firebase Auth backend
+        destination: 'https://nutriscan-1zg7u.firebaseapp.com/__/auth/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,7 +6,7 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration, read from environment variables
 const firebaseConfig: FirebaseOptions = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  authDomain: "eatinformed.amitdevx.tech",
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
